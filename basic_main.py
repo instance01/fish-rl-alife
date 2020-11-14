f0165d9785730a495e333880562263768137effb
Daniel Ratke <D.Ratke@campus.lmu.de>
2020-11-14 14:01:19 +0100

    Clean basic_main.

--- basic_main.py
+++ basic_main.py
@@ -2,23 +2,26 @@ from env.aquarium import Aquarium
 from env.animal_controller import DefaultSharkController as DShark
 
 
+EPISODES = 100
+
+
 if __name__ == '__main__':
+    env = Aquarium(
+        observable_sharks=3,
+        observable_fishes=3,
+        size=30,
+        max_fish=10,
+        torus=False,
+        fish_collision=True,
+        lock_screen=False,
+        max_steps=1000
+    )
 
-    EPISODES = 100
-    # build the environment
-    env = Aquarium(observable_sharks=3,
-                   observable_fishes=3,
-                   size=30,
-                   max_fish=10,
-                   torus=False,
-                   fish_collision=True,
-                   lock_screen=False,
-                   max_steps=300
-                   )
-
-    env.select_fish_types(Random_Fish=0,
-                          Turn_Away_Fish=1,
-                          Boid_Fish=0)
+    env.select_fish_types(
+        Random_Fish=0,
+        Turn_Away_Fish=5,
+        Boid_Fish=0
+    )
 
     env.select_shark_types(Shark_Agents=1)
 
@@ -27,10 +30,10 @@ if __name__ == '__main__':
         while not env.is_finished:
             shark_joint_action = {}
             for shark_name, observation in joint_shark_observation.items():
-                action = DShark.get_action(**env.prepare_observation_for_controller(observation))
+                action = DShark.get_action(
+                    **env.prepare_observation_for_controller(observation)
+                )
                 shark_joint_action[shark_name] = action
             new_state = env.step(shark_joint_action)
             joint_shark_observation = new_state[0]
             env.render()
-
-
