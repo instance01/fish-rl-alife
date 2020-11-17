eec1495c8415c676a25379f4d4ba6564bc7b723f
Daniel Ratke <D.Ratke@campus.lmu.de>
2020-11-17 19:21:03 +0100

    Small fix in basic_main.

--- basic_main.py
+++ basic_main.py
@@ -18,14 +18,15 @@ if __name__ == '__main__':
     )
 
     env.select_fish_types(
-        Random_Fish=0,
-        Turn_Away_Fish=5,
-        Boid_Fish=0
+        random_fish=0,
+        turn_away_fish=5,
+        boid_fish=0
     )
 
     env.select_shark_types(Shark_Agents=1)
 
     for training_episode in range(1, EPISODES + 1):
+        print(training_episode)
         joint_shark_observation = env.reset()
         while not env.is_finished:
             shark_joint_action = {}
@@ -35,5 +36,6 @@ if __name__ == '__main__':
                 )
                 shark_joint_action[shark_name] = action
             new_state = env.step(shark_joint_action)
+            print(new_state[2])
             joint_shark_observation = new_state[0]
             env.render()
