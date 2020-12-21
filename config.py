d1d74a8e3a7dc2e1e2574ad23c1cb3a83383ec97
Daniel Ratke <D.Ratke@campus.lmu.de>
2020-12-21 08:13:25 +0100

    Make sure all base configs use config '1' as base config to ensure backwards compatibility.

--- config.py
+++ config.py
@@ -7,18 +7,28 @@ class Config:
         with open('simulations.json', 'r') as f:
             self.cfg = json.load(f)
 
+    def _update_cfg(self, base_cfg, new_cfg):
+        # We support one level for now.
+        for k in new_cfg.keys():
+            if k == 'base_cfg' or k == 'desc':
+                continue
+            base_cfg[k].update(new_cfg[k])
+
     def get_cfg(self, cfg_id):
-        base_cfg = self.cfg[self.cfg[cfg_id].get('base_cfg', cfg_id)]
-        cfg = copy.deepcopy(base_cfg)
         if cfg_id not in self.cfg:
             raise Exception(
                 'Error: Key %s does not exist in simulations.json.' % cfg_id
             )
-        # We support one level for now.
-        for k in self.cfg[cfg_id].keys():
-            if k == 'base_cfg':
-                continue
-            cfg[k].update(self.cfg[cfg_id][k])
+
+        initial_base_cfg = self.cfg["1"]
+        base_cfg = self.cfg[self.cfg[cfg_id].get('base_cfg', cfg_id)]
+        # All base configs are based on config "1".
+        # This enables backwards compatibility when new options are added.
+        self._update_cfg(initial_base_cfg, base_cfg)
+
+        cfg = copy.deepcopy(initial_base_cfg)
+        self._update_cfg(cfg, self.cfg[cfg_id])
+
         cfg['cfg_id'] = cfg_id
         return cfg
 
