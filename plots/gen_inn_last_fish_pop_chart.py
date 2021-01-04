666ff58b62da8c805743d0029aad066a00520eaf
Daniel Ratke <D.Ratke@campus.lmu.de>
2021-01-04 20:23:19 +0100

    Update fish pop chart

--- plots/gen_inn_last_fish_pop_chart.py
+++ plots/gen_inn_last_fish_pop_chart.py
@@ -9,7 +9,8 @@ mpl.font_manager._rebuild()
 plt.rc('font', family='Raleway')
 
 
-with open('inn_last_fish_pop.pickle', 'rb') as f:
+# with open('../pickles/inn_last_fish_pop.pickle', 'rb') as f:
+with open('../pickles/t800_5fish_last_fish_pop.pickle', 'rb') as f:
     a = pickle.load(f)
 
 
@@ -18,25 +19,21 @@ def moving_average(x, w):
 
 
 a = moving_average(a, 100)
+print(len(a))
+
+plt.figure(figsize=(4, 3))
 plt.plot(a, linewidth=.8, c='#ffffff')
 plt.ylabel('Fish population at the end of an episode')
 plt.xlabel('Steps in million')
-plt.xticks([0, 1000, 2000, 3000, 4000, 5000, 6000], ['0', '2', '4', '6', '8', '10', '12'])
-# plt.axvspan(0, 600, facecolor='#990000', alpha=0.2)
-# plt.axvspan(600, 5100, facecolor='#009900', alpha=0.2)
-# plt.axvspan(5100, 5900, facecolor='#000099', alpha=0.2)
-# plt.axvspan(0, 600, facecolor='#344966', alpha=0.6)
-# plt.axvspan(600, 5100, facecolor='#2D7DD2', alpha=0.6)
-# plt.axvspan(5100, 5900, facecolor='#009440', alpha=0.7)
-plt.axvspan(0, 600, facecolor='#00622c', alpha=1.0)
-plt.axvspan(600, 5100, facecolor='#007a35', alpha=1.0)
-plt.axvspan(5100, 5900, facecolor='#009440', alpha=1.0)
-plt.axvline(600, color='black', linestyle='--' , linewidth=.2)
-plt.axvline(5100, color='black', linestyle='--', linewidth=.2)
+plt.xticks([0, 2000, 4000, 6000, 8000, 10000, 12000, 13980], ['0', '3', '6', '9', '12', '15', '18', '21'])
+plt.axvspan(0, 2000, facecolor='#00622c', alpha=1.0)
+plt.axvspan(2000, 4800, facecolor='#007a35', alpha=1.0)
+plt.axvspan(4800, 13980, facecolor='#009440', alpha=1.0)
+plt.axvline(2000, color='black', linestyle='--' , linewidth=.2)
+plt.axvline(4800, color='black', linestyle='--', linewidth=.2)
 plt.axhline(2, color='black', linestyle='--', linewidth=.5)
-# plt.title('a)                                 b)                                   c)')
-# plt.title('a                                   b                                    c ')
-plt.title('a                                                   b                                                c ')
-plt.xlim(0, 5900)
-plt.ylim(0, 12)
+plt.title(' a           b                              c                    ')
+plt.xlim(0, 13980)
+plt.ylim(0, 7)
+plt.tight_layout()
 plt.show()
