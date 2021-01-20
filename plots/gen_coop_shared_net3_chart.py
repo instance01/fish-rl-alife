53fc270082450c4915efbf7941d7770e0031072a
Daniel Ratke <D.Ratke@campus.lmu.de>
2021-01-20 13:12:59 +0100

    Add net3 SHARED version (pickles+chart), looks like it has worse performance..

--- plots/gen_coop_shared_net3_chart.py
+++ plots/gen_coop_shared_net3_chart.py
@@ -56,27 +56,16 @@ def _prep(data, failure=False, prefix='i10'):
 
 base_path = '../pickles/'
 failure = False
-with open(base_path + 'vd15_coop_net3.pickle', 'rb') as f:
+with open(base_path + 'vd15_coop_net3_shared.pickle', 'rb') as f:
     i5_data, i5_label_data = _prep(pickle.load(f), failure, prefix='vd15')
-with open(base_path + 'vd20_coop_net3.pickle', 'rb') as f:
-    i5_data2, i5_label_data2 = _prep(pickle.load(f), failure, prefix='vd20')
-with open(base_path + 'vd25_coop_net3.pickle', 'rb') as f:
+# with open(base_path + 'vd20_coop_net3.pickle', 'rb') as f:
+#     i5_data2, i5_label_data2 = _prep(pickle.load(f), failure, prefix='vd20')
+with open(base_path + 'vd25_coop_net3_shared.pickle', 'rb') as f:
     i5_data3, i5_label_data3 = _prep(pickle.load(f), failure, prefix='vd25')
-with open(base_path + 'vd30_coop_net3.pickle', 'rb') as f:
-    i5_data4, i5_label_data4 = _prep(pickle.load(f), failure, prefix='vd30')
-with open(base_path + 'vd35_coop_net3.pickle', 'rb') as f:
-    i5_data5, i5_label_data5 = _prep(pickle.load(f), failure, prefix='vd35')
-
-with open(base_path + 'vd15_coop_net3_normal_coop.pickle', 'rb') as f:
-    i10_data, i10_label_data = _prep(pickle.load(f), failure, prefix='vd15')
-with open(base_path + 'vd20_coop_net3_normal_coop.pickle', 'rb') as f:
-    i10_data2, i10_label_data2 = _prep(pickle.load(f), failure, prefix='vd20')
-with open(base_path + 'vd25_coop_net3_normal_coop.pickle', 'rb') as f:
-    i10_data3, i10_label_data3 = _prep(pickle.load(f), failure, prefix='vd25')
-with open(base_path + 'vd30_coop_net3_normal_coop.pickle', 'rb') as f:
-    i10_data4, i10_label_data4 = _prep(pickle.load(f), failure, prefix='vd30')
-with open(base_path + 'vd35_coop_net3_normal_coop.pickle', 'rb') as f:
-    i10_data5, i10_label_data5 = _prep(pickle.load(f), failure, prefix='vd35')
+# with open(base_path + 'vd30_coop_net3.pickle', 'rb') as f:
+#     i5_data4, i5_label_data4 = _prep(pickle.load(f), failure, prefix='vd30')
+# with open(base_path + 'vd35_coop_net3.pickle', 'rb') as f:
+#     i5_data5, i5_label_data5 = _prep(pickle.load(f), failure, prefix='vd35')
 
 
 def doit(i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3, i5_label_data3, i5_data4, i5_label_data4, i5_data5, i5_label_data5,):
@@ -86,15 +75,17 @@ def doit(i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3, i5_label_da
     x_labels = ['.025', '.03', '.035', '.04', '.05']
 
     cmap_mod = truncate_colormap('Greens', minval=.3, maxval=.99)
-    fig, (ax, ax2, ax3, ax4, ax5) = plt.subplots(1, 5, figsize=(14.5, 2.0), constrained_layout=True)
+    # fig, (ax, ax2, ax3, ax4, ax5) = plt.subplots(1, 5, figsize=(14.5, 2.0), constrained_layout=True)
+    fig, (ax, ax3) = plt.subplots(1, 2, figsize=(14.5, 2.0), constrained_layout=True)
     im = ax.imshow(i5_data, cmap=cmap_mod, vmin=0, vmax=1)
-    im2 = ax2.imshow(i5_data2, cmap=cmap_mod, vmin=0, vmax=1)
+    # im2 = ax2.imshow(i5_data2, cmap=cmap_mod, vmin=0, vmax=1)
     im3 = ax3.imshow(i5_data3, cmap=cmap_mod, vmin=0, vmax=1)
-    im4 = ax4.imshow(i5_data4, cmap=cmap_mod, vmin=0, vmax=1)
-    im5 = ax5.imshow(i5_data5, cmap=cmap_mod, vmin=0, vmax=1)
+    # im4 = ax4.imshow(i5_data4, cmap=cmap_mod, vmin=0, vmax=1)
+    # im5 = ax5.imshow(i5_data5, cmap=cmap_mod, vmin=0, vmax=1)
 
     # Colorbar
-    cbar = ax.figure.colorbar(im, ax=[ax, ax2, ax3, ax4, ax5], aspect=60)
+    # cbar = ax.figure.colorbar(im, ax=[ax, ax2, ax3, ax4, ax5], aspect=60)
+    cbar = ax.figure.colorbar(im, ax=[ax, ax3], aspect=60)
     cbar.ax.set_ylabel('Avg Cooperation Rate', rotation=-90, va="bottom")
     if failure:
         cbar.ax.set_ylabel('Avg Failure Rate', rotation=-90, va="bottom")
@@ -104,38 +95,38 @@ def doit(i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3, i5_label_da
     ax.set_yticks(np.arange(len(y_labels)))
     ax.set_xticklabels(x_labels)
     ax.set_yticklabels(y_labels)
-    ax2.set_xticks(np.arange(len(x_labels)))
-    ax2.set_yticks(np.arange(len(y_labels)))
-    ax2.set_xticklabels(x_labels)
-    ax2.set_yticklabels(y_labels)
+    # ax2.set_xticks(np.arange(len(x_labels)))
+    # ax2.set_yticks(np.arange(len(y_labels)))
+    # ax2.set_xticklabels(x_labels)
+    # ax2.set_yticklabels(y_labels)
     ax3.set_xticks(np.arange(len(x_labels)))
     ax3.set_yticks(np.arange(len(y_labels)))
     ax3.set_xticklabels(x_labels)
     ax3.set_yticklabels(y_labels)
-    ax4.set_xticks(np.arange(len(x_labels)))
-    ax4.set_yticks(np.arange(len(y_labels)))
-    ax4.set_xticklabels(x_labels)
-    ax4.set_yticklabels(y_labels)
-    ax5.set_xticks(np.arange(len(x_labels)))
-    ax5.set_yticks(np.arange(len(y_labels)))
-    ax5.set_xticklabels(x_labels)
-    ax5.set_yticklabels(y_labels)
+    # ax4.set_xticks(np.arange(len(x_labels)))
+    # ax4.set_yticks(np.arange(len(y_labels)))
+    # ax4.set_xticklabels(x_labels)
+    # ax4.set_yticklabels(y_labels)
+    # ax5.set_xticks(np.arange(len(x_labels)))
+    # ax5.set_yticks(np.arange(len(y_labels)))
+    # ax5.set_xticklabels(x_labels)
+    # ax5.set_yticklabels(y_labels)
     ax.set_ylabel('Killzone Radius', rotation=90, va="bottom")
     ax.set_xlabel('Shark speed', rotation=0, va="top")
-    ax2.set_xlabel('Shark speed', rotation=0, va="top")
+    # ax2.set_xlabel('Shark speed', rotation=0, va="top")
     ax3.set_xlabel('Shark speed', rotation=0, va="top")
-    ax4.set_xlabel('Shark speed', rotation=0, va="top")
-    ax5.set_xlabel('Shark speed', rotation=0, va="top")
+    # ax4.set_xlabel('Shark speed', rotation=0, va="top")
+    # ax5.set_xlabel('Shark speed', rotation=0, va="top")
 
     # Text annotations
     for i in range(len(y_labels)):
         for j in range(len(x_labels)):
             # ax.text(j, i, i10_label_data[i][j], ha="center", va="center", color="w")
             ax.text(j, i, i5_label_data[i][j], ha="center", va="center", color="w")
-            ax2.text(j, i, i5_label_data2[i][j], ha="center", va="center", color="w")
+            # ax2.text(j, i, i5_label_data2[i][j], ha="center", va="center", color="w")
             ax3.text(j, i, i5_label_data3[i][j], ha="center", va="center", color="w")
-            ax4.text(j, i, i5_label_data4[i][j], ha="center", va="center", color="w")
-            ax5.text(j, i, i5_label_data5[i][j], ha="center", va="center", color="w")
+            # ax4.text(j, i, i5_label_data4[i][j], ha="center", va="center", color="w")
+            # ax5.text(j, i, i5_label_data5[i][j], ha="center", va="center", color="w")
 
     plt.show()
     return fig
@@ -182,20 +173,8 @@ def doit_single(id_, i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3,
 
 
 print('DOING i5')
-fig = doit(i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3, i5_label_data3, i5_data4, i5_label_data4, i5_data5, i5_label_data5)
+fig = doit(i5_data, i5_label_data, None, None, i5_data3, i5_label_data3, None, None, None, None)
 if failure:
     fig.savefig("i5_coop_net3_failure.pdf", bbox_inches='tight')
 else:
     fig.savefig("i5_coop_net3.pdf", bbox_inches='tight')
-print('DOING i10 (normal coop)')
-fig = doit(i10_data, i10_label_data, i10_data2, i10_label_data2, i10_data3, i10_label_data3, i10_data4, i10_label_data4, i10_data5, i10_label_data5)
-if failure:
-    fig.savefig("i5_coop_net3_failure_normal_coop.pdf", bbox_inches='tight')
-else:
-    fig.savefig("i5_coop_net3_normal_coop.pdf", bbox_inches='tight')
-
-
-# print('DOING i5')
-# fig = doit_single('i5', i5_data, i5_label_data, i5_data2, i5_label_data2, i5_data3, i5_label_data3, i5_data4, i5_label_data4)
-# print('DOING i10')
-# fig = doit_single('i10', i10_data, i10_label_data, i10_data2, i10_label_data2, i10_data3, i10_label_data3, i10_data4, i10_label_data4)
